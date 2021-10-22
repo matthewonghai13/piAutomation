e0f0f9588561506e0dd58b8ad3f05d239cc0d6d3
Matthew <mattonghai@gmail.com>
2021-10-21 23:07:58 -0500

    init commit

---
+++ luluBot/bot.py
@@ -0,0 +1,38 @@
+import pyperclip
+import requests
+import random
+import string
+import time
+import timeit
+import sys
+import re
+from threading import Thread
+import os
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from webdriver_manager.chrome import ChromeDriverManager
+from faker import Faker
+from random import randrange
+from termcolor import colored
+
+def main():
+    chrome_options = Options()
+    
+    # TODO: fix not working in headless
+    # chrome_options.add_argument("--headless")
+    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    driver.get("https://shop.lululemon.com/p/men-shorts/Pace-Breaker-Short-5-Lined/_/prod10080318?color=0001&sz=XS")
+    # driver.get("https://shop.lululemon.com/p/men-shorts/Pace-Breaker-Short-5-Lined/_/prod10080318?color=0001&sz=M")
+    sleep(10)
+    if len(driver.find_elements_by_id("purchase-attributes-size-notification-error")) > 0:
+        print("Nothing Found.")
+    else:
+        print("In stock!")
+    driver.close()
+
+
+if __name__ == '__main__':
+    main()
