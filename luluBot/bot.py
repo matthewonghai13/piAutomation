cc96bcd04b24ef8fb6847a23f95f1f3ff548339e
Matthew <mattonghai@gmail.com>
2021-10-21 23:25:51 -0500

    platform print

--- luluBot/bot.py
+++ luluBot/bot.py
@@ -14,16 +14,18 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.utils import ChromeType
 from faker import Faker
 from random import randrange
 from termcolor import colored
+from sys import platform
 
 def main():
     chrome_options = Options()
-    
+    print(platform)
     # TODO: fix not working in headless
     # chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+    driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install(), options=chrome_options)
     driver.get("https://shop.lululemon.com/p/men-shorts/Pace-Breaker-Short-5-Lined/_/prod10080318?color=0001&sz=XS")
     # driver.get("https://shop.lululemon.com/p/men-shorts/Pace-Breaker-Short-5-Lined/_/prod10080318?color=0001&sz=M")
     sleep(10)
